111d9924a3aa84b2a8bac9f54fa7e1f887c5dd26
kylahiggins <kyla.higgns@outlook.com>
2023-02-21 20:37:13 +1300

    header build

--- kh_news_app/src/App.vue
+++ kh_news_app/src/App.vue
@@ -1,23 +1,32 @@
 <script setup>
 // import article_component from './components/article_component.vue'
-import header from './components/header_component.vue'
+import header_component from './components/header_component.vue'
 </script>
 
 <template>
   <header>
+  <header_component/>
 
   </header>
 
   <section class="article_display">
-    <h2>News Articles</h2>
+    <h3>News Articles</h3>
   </section>
 
-  <footer>
-   <h1>hello</h1>
-  </footer> 
 
 </template>
 
 <style scoped>
+
+*{
+  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  margin: 5px;
+}
+
+.article_display{
+  margin-top: 20px;
+  text-align: center;
+  display: flex;
+}
 
 </style>
---
+++ kh_news_app/src/components/header_component.vue
@@ -0,0 +1,76 @@
+<template>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <div class="header_container">
+        <div class="nav_item">
+            <span class="material-symbols-outlined">menu</span>
+        </div>
+
+        <div class="nav_item">
+            <div class="logo_img">
+                <img src="../assets/images/Asset 1.png" alt="news icon">
+            </div>
+        </div>
+
+        <div class="nav_item" @click="search_click = !search_click;">
+            <span class="material-symbols-outlined">search</span>
+        </div>
+    </div>
+
+
+<div v-if="search_click" class="search_container">
+        <div class="search_input">
+            <input type="text" placeholder="Search">
+        </div>
+    </div>
+
+
+    
+    
+</template>
+
+<style scoped>
+.header_container{
+    margin-top: 30px;
+    display: flex;
+    padding: 10px;
+}
+
+.nav_item{
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+}
+
+.logo_img img{
+    height: 30px;
+    object-fit: contain;
+}
+
+.search_container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+input{
+    border-radius: 20px;
+    padding: 10px;
+    width: 250px;
+    border: 1px solid rgb(107, 107, 107);
+    text-align: center;
+}
+</style>
+
+<script>
+export default {
+  data(){ // data itself starts
+    return{ // data variables starts
+    search_click : true
+    } // data variables end
+  }, // data itself ends
+  }
+</script>
+
+
