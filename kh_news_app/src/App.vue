7877d791bd3224b0d3ce33f4e4422e8cde52e723
kylahiggins <kyla.higgns@outlook.com>
2023-02-22 11:29:47 +1300

    Update App.vue

--- kh_news_app/src/App.vue
+++ kh_news_app/src/App.vue
@@ -36,3 +36,25 @@ import header_component from './components/header_component.vue'
 }
 
 </style>
+
+<script>
+export default {
+  data(){ // data itself starts
+    return{ // data variables starts
+      my_data_array:[]
+    } // data variables end
+  }, // data itself ends
+  methods:{ // funtions are written in methods
+    async data_fetch(){
+      const response = await fetch("https://newsapi.org/v2/everything?q=music&apiKey=a538540a4b4c4ea48ff8f0b4afb141ec");
+      const received_data = await response.json();
+      // at this point our data is available under received_data variable
+      console.log( received_data );
+      this.my_data_array = received_data;
+    }
+  }, // methods end
+  created(){ // created() is where initial code runs once
+    this.data_fetch();
+  } // end of created
+}
+</script>
