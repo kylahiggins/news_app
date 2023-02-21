4d5fa0908fac888310b5ec46b00c583946ddc240
kylahiggins <kyla.higgns@outlook.com>
2023-02-21 16:22:55 +1300

    setup

--- kh_news_app/src/App.vue
+++ kh_news_app/src/App.vue
@@ -1,3 +1,23 @@
+<script setup>
+// import article_component from './components/article_component.vue'
+import header from './components/header_component.vue'
+</script>
+
 <template>
-  <h1>hello</h1>
+  <header>
+
+  </header>
+
+  <section class="article_display">
+    <h2>News Articles</h2>
+  </section>
+
+  <footer>
+   <h1>hello</h1>
+  </footer> 
+
 </template>
+
+<style scoped>
+
+</style>
